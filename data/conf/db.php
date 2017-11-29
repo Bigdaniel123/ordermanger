08b350c84d43a9d07825b93801601f1dd501e12f
陈鹏杰 <260083304@qq.com>
2017-11-29 10:27:43 +0800

    修复添加和编辑功能

--- data/conf/db.php
+++ data/conf/db.php
@@ -2,12 +2,26 @@
 	/**
 	 * 配置文件
 	 */
+	// return array(
+	// 	'DB_TYPE' => 'mysql',
+	// 	'DB_HOST' => 'localhost',
+	// 	'DB_NAME' => 'daikuan',
+	// 	'DB_USER' => 'root',
+	// 	'DB_PWD' => 'root',
+	// 	'DB_PORT' => '3306',
+	// 	'DB_PREFIX' => 'cmf_',
+	// 	//密钥
+	// 	"AUTHCODE" => 'sUEIDIwJwIK5ikLyGF',
+	// 	//cookies
+	// 	"COOKIE_PREFIX" => 'ekagtH_',
+	// );
+
 	return array(
 		'DB_TYPE' => 'mysql',
-		'DB_HOST' => 'localhost',
-		'DB_NAME' => 'daikuan',
-		'DB_USER' => 'root',
-		'DB_PWD' => 'root',
+		'DB_HOST' => '47.96.180.17',
+		'DB_NAME' => 'xyd',
+		'DB_USER' => 'xyd',
+		'DB_PWD' => 'xyd1988',
 		'DB_PORT' => '3306',
 		'DB_PREFIX' => 'cmf_',
 		//密钥
